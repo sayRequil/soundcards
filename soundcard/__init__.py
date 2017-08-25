91b35023abaea7d59e53a66dcf73b6546956a84c
Zack P <zack@zacklearns.com>
2017-08-25 11:05:25 -0600

    Create __init__.py

---
+++ soundcard/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+packages = []
+
+class soundcard:
+  def __init__(name="",description="",filetype="mp4",sound="sound",package=["soundcard","sound"]):
+    # name: Name of the soundcard.
+    # description: A short description describing the soundcard.
+    # filetype: The type of sound file.
+    # package: The package name. Example for others than soundcard: ["mysound","sound"] Which would mean the folder it is in, and the name of the sound.
+    diri = package[0]
+    file = package[1] + filetype
+    path = os.path.join(package,file)
+    packages.append(path)
+    n = open("name.scd","a")
+    n.write(name)
+    d = open("desc.scd","a")

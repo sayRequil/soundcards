eae2bb450f0e04296abbb2c8c3bd7a39b32e738f
Zack P <zack@zacklearns.com>
2017-08-25 20:57:30 -0600

    Update __init__.py

--- soundcard/__init__.py
+++ soundcard/__init__.py
@@ -1,23 +1,40 @@
 import os
+import pygame
 
 packages = []
 
 class soundcard:
-  def __init__(name="",description="",filetype="mp4",sound="sound",package=["soundcard","sound"]):
+  def __init__(name="",description="",filetype="mp4",sound="sound",package=["soundcard","sound"],build=False):
     # name: Name of the soundcard.
     # description: A short description describing the soundcard.
     # filetype: The type of sound file.
     # package: The package name. Example for others than soundcard: ["mysound","sound"] Which would mean the folder it is in, and the name of the sound.
-    diri = package[0]
-    file = package[1] + filetype
-    path = os.path.join(package,file)
-    packages.append(path)
-    n = open(os.path.join(package[0],"name.scd"),"a")
-    n.write(name)
-    d = open(os.path.join(package[0],"desc.scd"),"a")
-    d.write(desc)
-    n.close()
-    d.close()
+    if build == False:
+      diri = package[0]
+      file = package[1] + filetype
+      path = os.path.join(package,file)
+      packages.append(path)
+      n = open(os.path.join(package[0],"name.scd"),"a")
+      n.write(name)
+      d = open(os.path.join(package[0],"desc.scd"),"a")
+      d.write(desc)
+      n.close()
+      d.close()
+      
+    if build == True:
+      i = 0
+      for pack in packages:
+        i = i+1
+        p = os.path.join("cards_","card" + i)
+        card = open(p,"a")
+        card.write(name + " " + description + " " + filetype + " " + sound + " " + package[0]) # Example: soundname sounddesc mp4 sound soundcard
     
-  def build():
-    for 
+  def play(name="")
+    for r, d, f in os.walk("cards_")
+      c = open(f,"r")
+      data = c.split()
+      if data[0] == name:
+        sound = data[3] + data[2]
+        package = data[4]
+        path = os.path.join(package,sound)
+        pygame.mixer.music.load(path)

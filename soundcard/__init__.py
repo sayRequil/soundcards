02dd9c5886975a11dc9768bdcade9d2ceae84adf
Zack P <zack@zacklearns.com>
2017-08-25 21:03:06 -0600

    Update __init__.py

--- soundcard/__init__.py
+++ soundcard/__init__.py
@@ -4,7 +4,7 @@ import pygame
 packages = []
 
 class soundcard:
-  def __init__(name="",description="",filetype="mp4",sound="sound",package=["soundcard","sound"],build=False):
+  def __init__(name="",description="",author="",filetype="mp4",sound="sound",package=["soundcard","sound"],build=False):
     # name: Name of the soundcard.
     # description: A short description describing the soundcard.
     # filetype: The type of sound file.
@@ -27,14 +27,16 @@ class soundcard:
         i = i+1
         p = os.path.join("cards_","card" + i)
         card = open(p,"a")
-        card.write(name + " " + description + " " + filetype + " " + sound + " " + package[0]) # Example: soundname sounddesc mp4 sound soundcard
+        card.write(name + " " + description + " " + author + "" + filetype + " " + sound + " " + package[0]) # Example: soundname sounddesc soundauthor mp4 sound soundcard
     
   def play(name="")
     for r, d, f in os.walk("cards_")
       c = open(f,"r")
       data = c.split()
       if data[0] == name:
-        sound = data[3] + data[2]
+        print("Playing " + data[0] + " by " + data[2]) # Credits
+        sound = data[4] + data[3]
         package = data[4]
         path = os.path.join(package,sound)
         pygame.mixer.music.load(path)
+        
